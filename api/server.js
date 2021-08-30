6405a6982b6ddde1f033716f72886144aa847956
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-08-30 15:18:33 -0600

    Nearly done.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,84 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Model = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.use(express.json());
+
+// module.exports = {}; // EXPORT YOUR SERVER instead of {}
+
+//[Post]
+server.post("/api/users", async (req, res) => {
+  try {
+    if (!req.body.name || !req.body.bio) {
+      res.status(400).json({
+        message: "Please provide name and bio for the user",
+      });
+    } else {
+      const { name, bio } = req.body;
+      const newUser = await Model.insert({ name, bio });
+      res.status(201).json(newUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+      error: err.message,
+    });
+  }
+});
+
+//[Get]
+
+server.get("/api/users", (req, res) => {
+  Model.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: err.message });
+    });
+});
+
+//[Get] get by ID
+server.get(`/api/users/:id`, async (req, res) => {
+  try {
+    const user = await Model.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be retrieved",
+      error: err.message,
+    });
+  }
+});
+
+//[Delete] delete by ID
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const user = await Model.remove(req.params.id);
+    if (!user) {
+      res.status(404).json({
+        message: "the user with the specified ID does not exist",
+      });
+    } else {
+      res.status(200).json({
+        message: "user deleted",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user information could not be retrieved",
+      error: err.message,
+    });
+  }
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server');
+const server = require("./api/server");
 
-const port = 5000;
+server.listen(5000, () => {
+  console.log("listening on port 5000");
+});
 
 // START YOUR SERVER HERE
